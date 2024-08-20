edf99ea2167d8cab49c1842ba82f27f05da3a076
shantelmakena <makenashantel10@gmail.com>
2024-08-20 13:15:48 +0300

    first commit

---
+++ app/src/main/java/com/example/makenasapp/navigation/AppNavHost.kt
@@ -0,0 +1,48 @@
+package com.example.makenasapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.makenasapp.ui.theme.screens.about.AboutScreen
+import com.example.makenasapp.ui.theme.screens.home.HomeScreen
+import com.example.makenasapp.ui.theme.screens.login.LoginScreen
+import com.example.makenasapp.ui.theme.screens.signup.SignupScreen
+import com.example.makenasapp.ui.theme.screens.splash.SplashScreen
+
+@Composable
+fun AppNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination:String = ROUT_SPLASH
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+
+        composable(ROUT_HOME) {
+            HomeScreen(navController = navController)
+        }
+        composable(ROUT_ABOUT) {
+            AboutScreen(navController = navController)
+        }
+
+        composable(ROUT_SPLASH) {
+            SplashScreen(navController = navController)
+        }
+
+        composable(ROUT_SIGNUP) {
+            SignupScreen(navController = navController)
+        }
+        composable(ROUT_LOGIN) {
+            LoginScreen(navController = navController)
+        }
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/makenasapp/navigation/Routes.kt
@@ -0,0 +1,8 @@
+package com.example.makenasapp.navigation
+
+const val ROUT_HOME="home"
+const val ROUT_ABOUT="about"
+const val ROUT_SPLASH="splash"
+const val ROUT_SIGNUP="signup"
+const val ROUT_LOGIN="login"
+
